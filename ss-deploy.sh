7b3b71b964ff6a0872c6438027242f05d9451f4b
David Norman <davidn@cgraphics.com>
2012-08-22 00:08:14 -0400

    Add uploadprogress pecl, increase APC shm, the order of installing aegir after drush backports.

--- ss-deploy.sh
+++ ss-deploy.sh
@@ -27,7 +27,8 @@ echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.list.d/j
 echo "deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/debian squeeze main" >> /etc/apt/sources.list.d/mariadb.list
 echo "deb-src http://ftp.osuosl.org/pub/mariadb/repo/5.5/debian squeeze main" >> /etc/apt/sources.list.d/mariadb.list
 echo "deb http://debian.aegirproject.org/ squeeze main" >> /etc/apt/sources.list.d/aegir.list
-echo "deb-src http://debian.aegirproject.org/ squeeze main" >> /etc/apt/sources.list/aegir.list
+echo "deb-src http://debian.aegirproject.org/ squeeze main" >> /etc/apt/sources.list.d/aegir.list
+echo "deb http://backports.debian.org/debian-backports squeeze-backports main" >> /etc/apt/sources.list.d/backports.list
 
 apt-get update
 apt-get --yes upgrade
@@ -44,15 +45,21 @@ apt-get -f --yes install apachetop build-essential apache2 apache2-threaded-dev 
 
 # installing PHP separately resolves a libmysqlclient18 lib conflict with MariaDB
 
+# Provides more recent versions of PHP
 wget -q -O - http://www.dotdeb.org/dotdeb.gpg | apt-key add -
 echo "deb http://packages.dotdeb.org squeeze all" >> /etc/apt/sources.list.d/dotdeb.list
 echo "deb-src http://packages.dotdeb.org squeeze all" >> /etc/apt/sources.list.d/dotdeb.list
 apt-get update
 
-apt-get -f --yes install php5 php5-apc php5-cgi php5-cli php5-common php5-curl php5-dev php5-gd php5-mcrypt php5-memcache php5-memcached php5-mysql php5-xmlrpc php-pear libapache2-mod-php5 aegir
+apt-get -f --yes install php5 php5-apc php5-cgi php5-cli php5-common php5-curl php5-dev php5-gd php5-mcrypt php5-memcache php5-memcached php5-mysql php5-xmlrpc php-pear libapache2-mod-php5
+apt-get --yes -t squeeze-backports install drush drush-make
+apt-get -f --yes install aegir aegir-hostmaster aegir-provision
 
 pear channel-discover pear.drush.org
-pear install drush/drush
+#pear install drush/drush
+pear update-channels
+pear upgrade-all
+pecl install uploadprogress
 
 if [ "`uname -m | grep 64`" ]
 then
@@ -63,10 +70,31 @@ else
 	dpkg -i /opt/mod-pagespeed-beta_current_i386.deb
 fi
 
+# Enables clean URLs in Drupal
 a2enmod rewrite
+
+# Allows Drupal to leverage client-side browser caches to store static content for a default of 2 weeks
 a2enmod expires
+
+# Google PageSpeed module to filter output in realtime as part of content optimization
 a2enmod pagespeed
 
+# Apache complains on start and restart if it doesn't have a complete server name. Debian doesn't attempt to set one by default.
+echo "ServerName $FQDN" > /etc/apache2/httpd.conf
+
+# Handles fancier progress meter in Drupal file uploads, but we will use uploadprogress PECL instead
+echo "apc.rfc1867 = Off" >> /etc/php5/apache2/php.ini
+
+# Increases default 32M to more easily handle more multiple Drupal installs
+echo "apc.shm_size = 64M" >> /etc/php5/apache2/php.ini
+
+# Checks the file time on every page load to see if the cache needs to be reset
+echo "apc.stat_ctime = On" >> /etc/php5/apache2/php.ini
+
+# Helps Drupal show a progres bar on file uploads
+echo "extension=uploadprogress.so" >> /etc/php5/apache2/php.ini
+
+# Allows .htaccess in the default /var/www virtualhost
 perl -pi -e 's/(\s+AllowOverride)\s+None$/\1 All/g' /etc/apache2/sites-available/default
 perl -pi -e 's/(\s+CustomLog \/var\/log\/apache2\/access\.log combined)$/#\1/g' /etc/apache2/sites-available/default
 perl -pi -e 's/(memory_limit =)\s+\d+M(.*?)$/\1 256M\2/g' /etc/php5/apache2/php.ini
