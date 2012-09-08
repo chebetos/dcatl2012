60fb0f0d1be52a275539260b4692b9f7f3e26527
David Norman <davidn@cgraphics.com>
2012-09-08 01:38:14 -0400

    Update drush from whatever the deb installs, alternate install method for jenkins plugins by wget instead of jenkins-cli.jar, install Sonar and its Jenkins plugin, bypass mod_security default rulesets for downloading public key, and fallback to set variables by Bash read if the Stackscript Linode method isn't used.

--- ss-deploy.sh
+++ ss-deploy.sh
@@ -10,12 +10,54 @@
 # DB_PASSWORD=
 # <UDF name="hostmaster_email" Label="Contact email address for the Aegir hostmaster">
 
+if [ -z "$SSHKEYURL" ]
+then
+  read -t 1 -n 1000 DISCARD
+  DISCARD=""
+
+  read -n 1 -p "URL of your SSH public key: " SSHKEYURL
+fi
+
+if [ -z "$HOSTNAME" ]
+then
+  read -t 1 -n 1000 DISCARD
+  DISCARD=""
+
+  read -n 1 -p "Set the hostname: " HOSTNAME
+fi
+
+if [ -z "$FQDN" ]
+then
+  read -t 1 -n 1000 DISCARD
+  DISCARD=""
+
+  read -n 1 -p "Set the fully qualified domain name: " FQDN
+fi
+
+if [ -z "$DB_PASSWORD" ]
+then
+  read -t 1 -n 1000 DISCARD
+  DISCARD=""
+
+  read -n 1 -p "Database root password: " DB_PASSWORD
+fi
+
+if [ -z "$HOSTMASTER_EMAIL" ]
+then
+  read -t 1 -n 1000 DISCARD
+  DISCARD=""
+
+  read -n 1 -p "Contact email address for the Aegir hostmaster: " HOSTMASTER_EMAIL
+fi
+
 PUBLICIP=$(ifconfig | grep -m 1 'inet addr:' | cut -d: -f2 | awk '{ print $1}');
 echo "$PUBLICIP $HOSTNAME $FQDN" >> /etc/hosts
 
 mkdir /root/.ssh/
 touch /root/.ssh/authorized_keys
-wget --no-check-certificate $SSHKEYURL --output-document=/tmp/ss-ssh.pub
+
+#set user agent string to bypass default mod_security rules
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64)" --no-check-certificate $SSHKEYURL --output-document=/tmp/ss-ssh.pub
 cat /tmp/ss-ssh.pub >> /root/.ssh/authorized_keys
 
 echo $HOSTNAME > /etc/hostname
@@ -28,6 +70,7 @@ wget -q http://debian.aegirproject.org/key.asc -O- | apt-key add -
 export DEBIAN_FRONTEND=noninteractive
 
 echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.list.d/jenkins.list
+echo "deb http://downloads.sourceforge.net/project/sonar-pkg/deb binary/" >> /etc/apt/sources.list.d/sonar.list
 echo "deb http://ftp.osuosl.org/pub/mariadb/repo/5.5/debian squeeze main" >> /etc/apt/sources.list.d/mariadb.list
 echo "deb-src http://ftp.osuosl.org/pub/mariadb/repo/5.5/debian squeeze main" >> /etc/apt/sources.list.d/mariadb.list
 echo "deb http://debian.aegirproject.org/ squeeze main" >> /etc/apt/sources.list.d/aegir.list
@@ -70,6 +113,11 @@ apt-get update
 apt-get -f --yes install php5 php5-apc php5-cgi php5-cli php5-common php5-curl php5-dev php5-gd php5-mcrypt php5-memcache php5-memcached php5-mysql php5-xmlrpc php-pear libapache2-mod-php5
 apt-get --yes -t squeeze-backports install drush drush-make
 
+# limited use of --force-yes, but this is an unauthenticated install
+apt-get --force-yes --yes install sonar
+# the sonar deb install scripts don't start the service
+/etc/init.d/sonar start
+
 echo "aegir-hostmaster aegir/db_password password ${DB_PASSWORD}" | debconf-set-selections
 echo "aegir-hostmaster aegir/db_user string root" | debconf-set-selections
 echo "aegir-hostmaster aegir/email string ${HOSTMASTER_EMAIL}" | debconf-set-selections
@@ -93,7 +141,9 @@ pear install --onlyreqdeps phpunit/phpcpd
 pear install --onlyreqdeps phpunit/phpdcd-beta
 
 # Using the backport drush deb to resolve dependency with aegir debs
-#pear install drush/drush
+# but this will give us the latest version over the deb's files
+pear install --alldeps Console_Table
+pear install drush/drush
 
 pecl install uploadprogress
 
@@ -146,19 +196,23 @@ perl -pi -e 's/(expose_php =)\s+On$/\1 Off/g' /etc/php5/cli/php.ini
 perl -pi -e 's/(mysql\.allow_persistent =)\s+On$/\1 Off/g' /etc/php5/cli/php.ini
 /etc/init.d/apache2 force-reload
 
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin analysis-collector
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin ansicolor
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin checkstyle
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin claim
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin clamav
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin disk-usage
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin dry
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin gerrit
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin gerrit-trigger
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin git
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin git-parameter
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin performance
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin pmd
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin view-job-filters
-java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 install-plugin ws-cleanup
+wget --no-check-certificate -O /var/lib/jenkins/plugins/analysis-collector.hpi https://updates.jenkins-ci.org/latest/analysis-collector.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/ansicolor.hpi https://updates.jenkins-ci.org/latest/ansicolor.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/checkstyle.hpi https://updates.jenkins-ci.org/latest/checkstyle.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/claim.hpi https://updates.jenkins-ci.org/latest/claim.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/clamav.hpi https://updates.jenkins-ci.org/latest/clamav.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/disk-usage.hpi https://updates.jenkins-ci.org/latest/disk-usage.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/dry.hpi https://updates.jenkins-ci.org/latest/dry.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/gerrit.hpi https://updates.jenkins-ci.org/latest/gerrit.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/gerrit-trigger.hpi https://updates.jenkins-ci.org/latest/gerrit-trigger.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/git.hpi https://updates.jenkins-ci.org/latest/git.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/git-parameter.hpi https://updates.jenkins-ci.org/latest/git-parameter.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/performance.hpi https://updates.jenkins-ci.org/latest/performance.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/pmd.hpi https://updates.jenkins-ci.org/latest/pmd.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/sonar.hpi http://updates.jenkins-ci.org/latest/sonar.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/view-job-filters.hpi http://updates.jenkins-ci.org/latest/view-job-filters.hpi
+wget --no-check-certificate -O /var/lib/jenkins/plugins/ws-cleanup.hpi https://updates.jenkins-ci.org/latest/ws-cleanup.hpi
+chown jenkins:nogroup /var/lib/jenkins/plugins/*.hpi
 java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080 safe-restart
+
+wget -O /opt/gerrit-2.4.2.war http://gerrit.googlecode.com/files/gerrit-2.4.2.war
